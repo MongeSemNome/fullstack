ff5f964341bf00fdb0809356b8b1dbc5498eaa69
Monge <gamelitico@gmail.com>
2024-10-04 20:13:13 -0300

    new website

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get( '/home', function () {
+    return view('landing');
+});
+Route::get( '/about', action: function () {
+    return view(view: 'about');
+});
+Route::get( '/services', function () {
+    return view('services');
+});
+Route::get( '/contact', function () {
+    return view('contact');
+});
+Route::get( '/docs', function () {
+    return view('welcome');
+});
