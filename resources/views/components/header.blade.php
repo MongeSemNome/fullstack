85c9e7956de0ee8ff0917a31cac020ed5dace7ed
Monge <gamelitico@gmail.com>
2024-10-04 22:27:43 -0300

    updates

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,6 @@
     <div class="container">
       <h1 class="display-3 mb-4">2FSoft Soluções Digitais</h1>
       <p class="lead mb-5">Seu time de desenvolvimento de soluções digitais!</p>
-      <a href="./contact.html" class="btn btn-primary btn-lg">Contate-nos</a>
+      <a href="/contact" class="btn btn-primary btn-lg">Contate-nos</a>
     </div>
   </header>
